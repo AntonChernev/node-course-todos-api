4064d6ce2d43af03baf76998a56c744c4f1fd21c
Anton Chernev <ss.a.chernev@gmail.com>
2017-08-07 11:50:34 +0300

    mongoose functionality exploration

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -68,7 +68,7 @@ describe('GET /todos', () => {
             .expect(200)
             .expect((res) => {
                 expect(res.body.todos.length).toBe(2);
-            })
+             })
             .end(done());
     });
 });
