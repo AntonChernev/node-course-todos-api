67a91a78efd7b92dff233556d0eb687cd02f1f09
Anton Chernev <ss.a.chernev@gmail.com>
2017-08-11 14:21:40 +0300

    Added POST /users

--- server/server.js
+++ server/server.js
@@ -19,7 +19,6 @@ app.post('/todos', (req, res) => {
         text: req.body.text
     });
     todo.save().then((doc) => {
-        console.log(doc);
         res.send(doc);
     }, (err) => {
         res.status(400).send(err);
@@ -92,6 +91,20 @@ app.patch('/todos/:id', (req, res) => {
             res.status(400).send();
         });
 });
+
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    })
+    .catch((err) => {
+        res.status(400).send(err);
+    });
+})
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
